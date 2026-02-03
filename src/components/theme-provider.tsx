bfc73a7b5c3ec3bd08847e4ba0e325dcdcc342ca
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2026-02-02 22:50:03 -0800

    refactor: remove unused imports from ThemeProvider component

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,8 +3,6 @@
 import React, { useEffect } from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
-import { StatusBar, Style } from '@capacitor/status-bar';
-
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
